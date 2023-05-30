e9a8790260164e1a4a83e54793bcc062cebd0bf8
Alex <amkulikov@gmail.com>
2023-05-31 00:29:25 +0300

    Python_course_lesson4

--- .Python_Course/Lesson3/lesson3.py
+++ .Python_Course/Lesson3/lesson3.py
@@ -126,6 +126,75 @@ for i in list:
 print(result) # решение не полное. Не выведено второе наиболее 
               # близкое число, при условии равенства его с первым.
 
+######
+
+
+
+n = int(input())
+list_1 = list()
+for i in range(n):
+    x = int(input())
+    list_1.append(x)
+
+k = int(input())
+m = abs(k - list_1[0])  # модуль числа
+number = list_1[0]
+for i in range(1, len(list_1)):
+    if m > abs(list_1[i] - k):
+        m = abs(list_1[i] - k)
+        number = list_1[i]
+print(number)
+
+## еще
+# Задача №18. Требуется найти в массиве A[1..N] самый близкий по величине элемент к заданному числу X.
+from random import randint
+
+N = int(input("Введите число N: "))
+inputList = [randint(1, N) for i in range(N)]
+print(inputList)
+X = int(input("Введите число X: "))
+
+diffP = 0
+oldDiffP = max(inputList)
+diffN = 0
+oldDiffN = max(inputList)
+resNumP = 0
+resNumN = 0
+for num in inputList:
+    diff = X - num
+
+    if diff > 0:
+        diffP = diff
+    elif diff < 0:
+        diffN = diff
+
+    if abs(diffP) < abs(oldDiffP):
+        resNumP = num
+    oldDiffP = diffP
+
+    if abs(diffN) < abs(oldDiffN):
+        resNumN = num
+    oldDiffN = diffN
+print(resNumP)
+print(resNumN)
+
+
+
+########
+
+
+
+k = int(input())
+m = abs(k - list_1[0])  # модуль разности
+numbers = []  # список для хранения чисел
+
+for i in range(len(list_1)):
+    if abs(list_1[i] - k) <= m:
+        if abs(list_1[i] - k) < m:
+            numbers = []  # очищаем список, если нашли число ближайшее, чем предыдущее
+            m = abs(list_1[i] - k)
+        numbers.append(list_1[i])
+
 
 # Задача 20: В настольной игре Скрабл (Scrabble) каждая буква имеет определенную
 # ценность. В случае с английским алфавитом очки распределяются так:
@@ -147,5 +216,79 @@ print(result) # решение не полное. Не выведено втор
 # Напишите программу, которая вычисляет стоимость введенного пользователем слова.
 # Будем считать, что на вход подается только одно слово, которое содержит либо только
 # английские, либо только русские буквы.
-создать библиотеку, ключи - буквы, содержание - стоимость. делать запрос 
-на стоимость букв в слове и суммировать. Не успеваю...
+# создать библиотеку, ключи - буквы, содержание - стоимость. делать запрос 
+# на стоимость букв в слове и суммировать. Не успеваю...
+
+dict_1 = {'AEIOULNSTRАВЕИНОРСТ': 1, 'DGДКЛМПУ': 2, 'BCMPБГЁЬЯ': 3, 'FHVWYЙЫ': 4, 'KЖЗХЦЧ': 5, 'JXШЭЮ': 8, 'QZФЩЪ': 10} 
+word = list(input('Введите слово буквами верхнего регистра: '))
+price = 0
+for i in word:
+    for key in dict_1:
+        if i in key:
+            price = price + dict_1.get(key)
+print(f'Стоимость слова: {price}.')
+
+#####
+
+
+Игорь Москалев
+
+onePoints = dict.fromkeys(['А', 'В', 'Е', 'И', 'Н', 'О', 'Р', 'С', 'Т'], 1)
+twoPoints = dict.fromkeys(['Д', 'К', 'Л', 'М', 'П', 'У'], 2)
+threePoints = dict.fromkeys(['Б', 'Г', 'Ё', 'Ь', 'Я' ], 3)
+fourPoints = dict.fromkeys(['Й', 'Ы'], 4)
+fivePoints = dict.fromkeys(['Ж', 'З', 'Х', 'Ц', 'Ч'], 5)
+eightPoints = dict.fromkeys(['Ш', 'Э', 'Ю'], 8)
+tenPoints = dict.fromkeys(['Ф', 'Щ', 'Ъ'], 10)
+mergedDict = onePoints | twoPoints | threePoints | fourPoints | fivePoints | eightPoints | tenPoints 
+
+userText = list(input("Введите одно слово ").upper())
+sum = 0
+for i in userText:
+    sum += mergedDict[i]
+print(sum)
+
+
+###########
+
+
+eng = "qwertyuiopasdfghjklzxcvbnm"
+
+rus = "йцукенгшщзхъфывапролджэячсмитьбюё"
+
+list_English = {
+    1: "AEIOULNSTR",
+    2: "DG",
+    3: "BCMP",
+    4: "FHVWY",
+    5: "K",
+    8: "JX",
+    10: "QZ",
+}
+list_Russian = {
+    1: "АВЕИНОРСТ",
+    2: "ДКЛМПУ",
+    3: "БГЁЬЯ",
+    4: "ЙЫ",
+    5: "ЖЗХЦЧ",
+    8: "ШЭЮ",
+    10: "ФШЪ",
+}
+
+word = input("Введите слово на русском или английском языке: ")
+
+if word[0].lower() in eng:
+    summa = 0
+    for letter in word:
+        for key, value in list_English.items():
+            if letter.upper() in value:
+                summa += key
+    print(f"стоимость введенного английского слова = {summa}")
+else:
+    if word[0].lower() in rus:
+        summa = 0
+        for letter in word:
+            for key, value in list_Russian.items():
+                if letter.upper() in value:
+                    summa += key
+    print(f"стоимость введенного русского слова = {summa}")
